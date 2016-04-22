27ac3d4e5f316d1bfef6a2b6290a9a9885c7890a
Angus Fretwell <angusfretwell@me.com>
2016-04-23 00:08:59 +1000

    test: ensure item numeric validation

--- test.js
+++ test.js
@@ -2,17 +2,25 @@ import 'should';
 import ThreePoint from './source';
 
 describe('three-point', () => {
-  it('should calculate optimistic, pessimistic, likely', () => {
-    const estimate = new ThreePoint([
-      {
-        optimistic: 4,
-        pessimistic: 16,
-        likely: 8,
-      },
-    ], 0.90);
+  it('should calculate optimistic, pessimistic, expected estimates', () => {
+    const estimate = new ThreePoint([{
+      optimistic: 4,
+      pessimistic: 16,
+      likely: 8,
+    }], 0.90);
 
     estimate.expected.should.equal(8.7);
     estimate.optimistic.should.equal(5.4);
     estimate.pessimistic.should.equal(12);
+  });
+
+  it('should accept only numeric values for tasks', () => {
+    (() => {
+      new ThreePoint([{ // eslint-disable-line no-new
+        optimistic: 'fail',
+        pessimistic: 16,
+        likely: 8,
+      }]);
+    }).should.throw();
   });
 });
