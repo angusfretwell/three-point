b90989229c6e7f33868f27a18a543bb7cafdc266
Angus Fretwell <angusfretwell@me.com>
2016-04-22 23:34:55 +1000

    test: rudimentary test

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
 import 'should';
-import module from './source';
+import ThreePoint from './source';
 
-describe('module', () => {
-  it('default', () => {
-    module.should.be.true();
+describe('three-point', () => {
+  it('should calculate optimistic, pessimistic, likely', () => {
+    const estimate = new ThreePoint([
+      {
+        optimistic: 4,
+        pessimistic: 16,
+        likely: 8,
+      },
+    ], 0.90);
+
+    estimate.expected.should.equal(8.7);
+    estimate.pessemistic.should.equal(5.4);
+    estimate.optimistic.should.equal(12);
   });
 });
