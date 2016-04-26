4afce0a85b132a959e22ea04795e65b7a8c8b36a
Angus Fretwell <angusfretwell@me.com>
2016-04-26 12:56:02 +1000

    test: custom confidence level

--- test.js
+++ test.js
@@ -14,6 +14,18 @@ describe('three-point', () => {
     estimate.pessimistic.should.equal(12);
   });
 
+  it('should allow confidence to be customised', () => {
+    const estimate = new ThreePoint([{
+      optimistic: 4,
+      pessimistic: 16,
+      likely: 8,
+    }], 0.997);
+
+    estimate.expected.should.equal(8.7);
+    estimate.optimistic.should.equal(2.7);
+    estimate.pessimistic.should.equal(14.6);
+  });
+
   it('should accept only numeric values for items', () => {
     (() => {
       new ThreePoint([{ // eslint-disable-line no-new
